2bd57610573b61031ec53015989ff203841e7062
Eresia <bastien.lepesant@gmail.com>
2016-04-23 19:39:38 +0200

    Ajout du Make pour upload sur l'arduino

--- arduino/testMotor.cpp
+++ arduino/testMotor.cpp
@@ -7,14 +7,14 @@ int leftMotor = 6;
 int rightMotor = 7;
 
 void setup() {
-	pinMode(leftMotor, OUTPUT);
-	pinMode(rightMotor, OUTPUT);
+        pinMode(leftMotor, OUTPUT);
+        pinMode(rightMotor, OUTPUT);
 }
 
 void loop() {
-	digitalWrite(leftMotor, HIGH);
-	digitalWrite(rightMotor, HIGH);
-	delay(100);
-	digitalWrite(rightMotor, LOW);
-	delay(100);
+        digitalWrite(leftMotor, HIGH);
+        digitalWrite(rightMotor, HIGH);
+        delay(100);
+        digitalWrite(rightMotor, LOW);
+        delay(100);
 }
